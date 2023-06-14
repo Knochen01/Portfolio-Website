1b280243052d67643f33bf4a74fac6203a6a096c
Marian Primak <knochen2@gmail.com>
2023-06-14 14:47:05 -0700

    some minor changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
-    return (
-        <div name="contact" className="w-ful h-screen bg-[#0a192f] flex justify-center items-center p-4">
-            <form method="POST" action="https://getform.io/f/9104010e-38be-49fc-a2be-d6227c74a593" className="flex flex-col max-w-[600px] w-full">
-                <div className="pb-8">
-                    <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">Contact</p>
-                    <p className="text-gray-300 py-4">// Submit the form below or send me an email - knochen2@gmail.com</p>
-                </div>  
-                <input type="text" placeholder="name" name="name" className="bg-[#ccd6f6]"/>
-                <input type="email" placeholder="Email" name="email" className="my-4 p-2 bg-[#ccd6f6]"/>
-                <textarea className='bg-[#ccd6f6]' name="message" rows="10" placeholder="Message"></textarea>
-                <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">Let's Collaborate</button>
-            </form>
-            
+  const handleSubmit = (e) => {
+    console.log("Form submitted");
+    // Reset form inputs
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((previousData) => ({
+      ...previousData,
+      [name]: value,
+    }));
+  };
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  return (
+    <div
+      name="contact"
+      className="w-ful h-screen bg-[#0a192f] flex justify-center items-center p-4"
+    >
+      <form
+        method="POST"
+        action="https://getform.io/f/9104010e-38be-49fc-a2be-d6227c74a593"
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">
+            Submit the form below or send me an email - knochen2@gmail.com
+          </p>
         </div>
-    )
-}
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          className="bg-[#ccd6f6]"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          className="my-4 p-2 bg-[#ccd6f6]"
+        />
+        <textarea
+          className="bg-[#ccd6f6]"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button
+          className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center"
+          onClick={handleSubmit}
+        >
+          Let's Collaborate
+        </button>
+      </form>
+    </div>
+  );
+};
 export default Contact;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,36 @@
 import React from "react";
-import {HiArrowNarrowRight} from 'react-icons/hi';
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link as ScrollLink } from "react-scroll";
 
 const Home = () => {
-    return (
-        <div name="home" className="w-full h-screen bg-[#0a192f]">
-
-            {/* Container */}
-            <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-                <p className="text-pink-600">My Name is</p>
-                <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6] ">Marian Primak</h1>
-                <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I am a Front-End-Developer</h2>
-                <p className="text-[#8892b0] py-4 max-w-[700px]">I am a Front-End Developer specializing in buidling exceptional digital experiecnes. Currently, I am focused on building responsive web applications.</p>
-                <div>
-                    <button className=" group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 ">View Work
-                        <span className="group-hover:rotate-90 duration-300">
-                        <HiArrowNarrowRight className="ml-3"/>
-                        </span>
-                    </button>
-                   
-                </div>
-                
-            </div>
-
-        </div>
-    )
-}
-export default Home;  
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">My Name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6] ">
+          Marian Primak
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I am a Front-End-Developer
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I am a Front-End Developer specializing in buidling exceptional
+          digital experiecnes. Currently, I am focused on building responsive
+          web applications.
+        </p>
+        <ScrollLink to="work" smooth={true} duration={500}>
+          <div>
+            <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 ">
+              View Work
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </button>
+          </div>
+        </ScrollLink>
+      </div>
+    </div>
+  );
+};
+export default Home;
